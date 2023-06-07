d05dab21078e7e54b771c3025bc436fb4732edad
jbsankarlal <jbsankarlal@gmail.com>
2023-06-07 14:57:53 +0530

    last vendor update

--- frontend/src/Pages/vendor-Pages/vendor-login/vendorLogin.jsx
+++ frontend/src/Pages/vendor-Pages/vendor-login/vendorLogin.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../../Components/vendor/navbar/Navbar'
-import VendorLoggin from '../../../Components/vendor/vendor-log/VendorLoggin'
+import VendorLoggin from '../../../Components/vendor/vendor-log/vendorLoggin'
 
 const VendorLogin = () => {
     return (
